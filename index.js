5d6cec5e7f390cac24ec7c5335cc138cf05ed2d9
rahman.muraman <rahman.muraman@gmail.com>
2024-07-05 14:25:54 +0700

    refactor

--- index.js
+++ index.js
@@ -1,221 +1,44 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { books, members, loans } from "./_db.js";
+import { typeDefs } from "./schemas/typeDefs.js";
+import { resolvers } from "./resolvers/index.js";
+import { db } from "./config/db.js";
+import dotenv from "dotenv";
 
-// type Book Schema
-// type Query Query Schema nya
-// resolvers Fungsi untuk menangani query
+dotenv.config();
 
-const typeDefs = `#graphql
-    type Book {
-        id:ID!
-        name:String!
-        author:String!
-        year:Int!
-        available:Boolean!
-        loans:[Loan!]
-    }
-
-    type Member{
-        id:ID!
-        name:String!
-        email:String!
-        loans:[Loan!]
-    }
-
-    type Loan{
-        id:ID!
-        book:Book!
-        member:Member!
-        created_at:String!
-        updated_at:String!
-    }
-
-    type Query{
-        hello: String  
-        books: [Book!]!  
-        book(id:ID!): Book!
-        members: [Member!]!
-        member(id:ID!): Member!
-        loans: [Loan!]!
-        loan(id:ID!): Loan!
-    }
-
-    type Mutation{
-        createBook(bookInput: BookInput!): Book
-        updateBook(id:ID!, editBookInput: editBookInput! ): Book
-        deleteBook(id:ID!): [Book]
-        createMember(memberInput: MemberInput!): Member
-        updateMember(id:ID!, editMemberInput: editMemberInput!): Member
-        deleteMember(id:ID!): [Member]
-        createLoan(loanInput: LoanInput!): Loan
-        updateLoan(id:ID!, editLoanInput: editLoanInput!): Loan
-        deleteLoan(id:ID!): [Loan]
-    }
-
-    input BookInput{
-        name:String!
-        author:String!
-        year:Int!
-        available:Boolean!
-    }
-
-    input MemberInput{
-        name:String!
-        email:String!
-    }
-
-    input LoanInput{
-        book_id:ID!
-        member_id:ID!
-    }
-
-    input editBookInput{
-        name:String
-        author:String
-        year:Int
-        available:Boolean
-    }
-
-    input editMemberInput{
-        name:String
-        email:String
-    }
-
-    input editLoanInput{
-        book_id:ID
-        member_id:ID
-    }
-`;
-
-const resolvers = {
-  // query
-  Query: {
-    hello: () => "Hello World",
-    books: () => books,
-    book: (_, args) => {
-      return books.find((book) => book.id === args.id);
-    },
-    members: () => members,
-    member: (_, args) => {
-      return members.find((member) => member.id === args.id);
-    },
-    loans: () => loans,
-    loan: (_, args) => {
-      return loans.find((loan) => loan.id === args.id);
-    },
-  },
-
-  // related data
-  Book: {
-    loans: (parent) => {
-      return loans.filter((loan) => loan.book_id === parent.id);
-    },
-  },
-
-  Member: {
-    loans: (parent) => {
-      return loans.filter((loan) => loan.member_id === parent.id);
-    },
-  },
-  Loan: {
-    book: (parent) => {
-      return books.find((book) => book.id === parent.book_id);
-    },
-    member: (parent) => {
-      return members.find((member) => member.id === parent.member_id);
-    },
-  },
-
-  // mutation
-  Mutation: {
-    createBook: (_, args) => {
-      let book = {
-        ...args.bookInput,
-        id: (books.length + 1).toString(),
-      };
-
-      books.push(book);
-      return book;
-    },
-
-    updateBook: (_, args) => {
-      let bks = books.map((b) => {
-        if (b.id === args.id) {
-          return { ...b, ...args.editBookInput };
-        }
-
-        return b;
-      });
-
-      return bks.find((book) => book.id === args.id);
-    },
-
-    deleteBook: (_, args) => {
-      return books.filter((book) => book.id !== args.id);
-    },
-
-    createMember: (_, args) => {
-      let member = {
-        ...args.memberInput,
-        id: (members.length + 1).toString(),
-      };
-
-      members.push(member);
-      return member;
-    },
-
-    updateMember: (_, args) => {
-      let mbrs = members.map((m) => {
-        if (m.id === args.id) {
-          return { ...m, ...args.memberInput };
-        }
-
-        return m;
-      });
-
-      return mbrs.find((member) => member.id === args.id);
-    },
-
-    deleteMember: (_, args) => {
-      return members.filter((member) => member.id !== args.id);
-    },
-
-    createLoan: (_, args) => {
-      let loan = {
-        ...args.loanInput,
-        id: (loans.length + 1).toString(),
-      };
-
-      loans.push(loan);
-      return loan;
-    },
-
-    updateLoan: (_, args) => {
-      let lns = loans.map((l) => {
-        if (l.id === args.id) {
-          return { ...l, ...args.loanInput };
-        }
-
-        return l;
-      });
-
-      return lns.find((loan) => loan.id === args.id);
-    },
-
-    deleteLoan: (_, args) => {
-      return loans.filter((loan) => loan.id !== args.id);
-    },
-  },
+const initializeDB = async () => {
+  try {
+    await db.sync({ alter: true });
+    console.log("db created");
+  } catch (error) {
+    console.error("error", error);
+  }
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const startServer = async () => {
+  // type Book Schema
+  // type Query Query Schema nya
+  // resolvers Fungsi untuk menangani query
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-console.log(`server running at  ${url}`);
+  try {
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: process.env.PORT || 4000 },
+    });
+    console.log("🚀  Server ready at:", url);
+  } catch (error) {
+    console.error("Error starting server", error);
+  }
+};
+
+const initializationApp = async () => {
+  await db.authenticate();
+  await startServer();
+};
+
+initializationApp();
